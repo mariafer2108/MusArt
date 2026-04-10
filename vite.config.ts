bf6133569a4661b0c73cd5f7dc1f440fb902558b
mariafer2108 <navarromunoz.mariafer@gmail.com>
2026-04-10 12:20:21 -0400

    Dev: always proxy /api to Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5178',
+        target: 'https://musart.vercel.app',
         changeOrigin: true
       }
     }
